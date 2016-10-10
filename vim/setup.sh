ad1098a2aac7b9c5a0cbf1851a73cd5839cf9fc3
Shunsuke Tsukuda <shunsuke.tsukuda.1130@gmail.com>
2016-10-10 15:07:04 +0900

    add: setup.sh(for Linux)

---
+++ vim/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ ! -e ~/.vim ]; then
+  mkdir ~/.vim
+  mkdir ~/.vim/tmp
+  chmod 700 ~/.vim/tmp
+fi
+
+ln -sfv ~/.vimrc        ./.vimrc
+ln -sfv ~/filetype.vim  ./filetype.vim
+ln -sfv ~/.vim/after    ./after
+ln -sfv ~/.vim/rc       ./rc
