acd9d3a9ea3754a7a2fe3745e2d9f00de63e0ada
Shunsuke Tsukuda <shunsuke.tsukuda.1130@gmail.com>
2016-10-13 02:31:43 +0900

    fix setup.sh

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
+DOT='git.repos/dotfiles/vim'
+
 if [ ! -e ~/.vim ]; then
   mkdir ~/.vim
   mkdir ~/.vim/tmp
   chmod 700 ~/.vim/tmp
 fi
 
-ln -sfv ./.vimrc       ~/.vimrc
-ln -sfv ./filetype.vim ~/filetype.vim  
-ln -sfv ./after        ~/.vim/after    
-ln -sfv ./rc           ~/.vim/rc       
+ln -sfv  $HOME/$DOT/.vimrc       ~/.vimrc           
+ln -sfv  $HOME/$DOT/filetype.vim ~/.vim/filetype.vim
+ln -sfvn $HOME/$DOT/after        ~/.vim/after       
+ln -sfvn $HOME/$DOT/rc           ~/.vim/rc          
